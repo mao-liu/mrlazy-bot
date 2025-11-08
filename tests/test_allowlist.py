fa817e37fbb448f9b342b6255b23255dfe0e9af0
mao <1684060+mao-liu@users.noreply.github.com>
2025-11-08 20:25:42 +1100

    removed tests - we will need lots of changes

--- tests/test_allowlist.py
+++
@@ -1,8 +0,0 @@
-from mrlazy_bot.local_poller.allowlist import CommandAllowlist
-
-
-def test_load_allowlist_example():
-    allowlist = CommandAllowlist.load_from_yaml("config/commands.example.yaml")
-    assert allowlist.get("echo") is not None
-
-
